9a5dd7edaaa06a92dfe110d872de5715c356670c
Marco Hugentobler <marco.hugentobler@sourcepole.ch>
2018-08-17 17:11:22 +0200

    Write other files (except the ones requiring SurveyProperties)

--- transectsurveydialog.py
+++ transectsurveydialog.py
@@ -42,6 +42,12 @@ class TransectSurveyDialog( QtWidgets.QDialog, FORM_CLASS ):
         fillAttributeComboBox( self.mStrataIdAttributeComboBox,  layer )
         
     def createSample( self ):
+        
+        #survey properties
+        '''surveyProps = SurveyProperties( self )
+        if surveyProps.exec_() == QDialog.Rejected:
+            return'''
+        
         #save dir
         fileDialog = QFileDialog(  self,  QCoreApplication.translate( 'SurveyDesignDialog', 'Select output directory for result files' )  )
         fileDialog.setFileMode( QFileDialog.Directory )
@@ -74,6 +80,53 @@ class TransectSurveyDialog( QtWidgets.QDialog, FORM_CLASS ):
         transectSample = TransectSample(  strataMapLayer, strataId , strataMinDistance, strataNSamplePoints, minDistanceUnits, baselineMapLayer, True,
         '', outputPointShape, outputLineShape,  usedBaselineShape,  self.mMinimumTransectLengthSpinBox.value(),  -1.0,  -1.0 )
         transectSample.createSample()
+        
+        #open line shape and create a dict station_id / bearing
+        #first find out 'station_co' attribute name
+        transectLineLayer = QgsVectorLayer( outputLineShape,  'transectLineLayer',  'ogr' )
+        fields = transectLineLayer.fields()
+        stationCodeFieldName = "station_co"
+        for field in fields:
+            if field.name().startswith( "station_co" ):
+                stationCodeFieldName = field.name()
+                break
+        
+        bearingDict = {}
+        
+        transectLineIt = transectLineLayer.getFeatures()
+        for transectLineFeature in transectLineIt:
+            station_co = transectLineFeature.attribute( stationCodeFieldName )
+            bearing = transectLineFeature.attribute( 'bearing' )
+            bearingDict[station_co] = bearing
+            
+        #write bearing into output point shape
+        transectPointLayer = QgsVectorLayer( outputPointShape,  'transectPointLayer',  'ogr' )
+        transectPointLayer.startEditing()
+        transectPointLayer.addAttribute( QgsField( 'bearing',  QVariant.Double,  "Double" ) )
+        bearingIndex = transectPointLayer.fields().indexFromName( 'bearing' )
+        transectPointIt = transectPointLayer.getFeatures()
+        for transectPointFeature in transectPointIt:
+            station_co = transectPointFeature.attribute( stationCodeFieldName )
+            transectPointLayer.changeAttributeValue( transectPointFeature.id(),  bearingIndex,  bearingDict[ station_co] )
+        transectPointLayer.commitChanges()
+        
+        #write gpx file
+        gpxFileInfo = QFileInfo( outputPointShape )
+        gpxFileName = gpxFileInfo.path() + '/' + gpxFileInfo.baseName() + '.gpx'
+        writePointShapeAsGPX( outputPointShape,  stationCodeFieldName, 'bearing',   gpxFileName )
+        
+        #write csv files
+        #Survey.csv
+        '''writeSurveyCSV( fileDialog.selectedFiles()[0],  surveyProps.survey(),  surveyProps.projectCode(), surveyProps.date_s() , surveyProps.date_f(),  surveyProps.contactName(),  surveyProps.areas(), surveyProps.mainspp(),  surveyProps.comments() )
+        transectLayer = QgsVectorLayer( outputLineShape,  "transect",  "ogr" )
+        writeStationCSV( saveDir,  transectLayer, "stratum_id",  "station_id",  surveyProps.survey() )
+        writeStratumCSV( fileDialog.selectedFiles()[0], self.stratumLayer(), self.mStrataIdAttributeComboBox.currentText(),  surveyProps.survey() )
+        writeStratumBoundaryCSV( fileDialog.selectedFiles()[0], self.stratumLayer(), self.mStrataIdAttributeComboBox.currentText(),  surveyProps.survey() )'''
+        writeCatchCSV( saveDir )
+        writeLengthCSV( saveDir )
+        
+        self.iface.addVectorLayer( outputLineShape, 'transects', 'ogr' )
+        self.iface.addVectorLayer( outputPointShape,  'transect_stations',  'ogr' )
         
     def stratumLayer(self):
         comboIndex = self.mStrataLayerComboBox.currentIndex()
